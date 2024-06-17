cf464f43c88c8444a061d74d9faed0d2f9a2a4f6
kom-senapati <komnoob123@gmail.com>
2024-06-17 17:17:47 +0530

    code: added avatars for gemini

--- src/app/gemini/page.tsx
+++ src/app/gemini/page.tsx
@@ -36,7 +36,7 @@ const Gemini: React.FC = () => {
     setTimeout(() => {
       setMessages((prevMessages) => [
         ...prevMessages,
-        { text: "AI reply...", type: "ai" },
+        { text: "Imagine cool AI reply 😎", type: "ai" },
       ]);
     }, 500);
   };
@@ -52,7 +52,7 @@ const Gemini: React.FC = () => {
   };
 
   return (
-    <div className="bg-gradient-to-br from-black via-[#1f1f1f] to-black flex flex-col items-center justify-between h-screen p-3 px-52">
+    <div className="bg-gradient-to-br from-black via-[#1f1f1f] to-black flex flex-col items-center justify-between h-screen p-3 px-5 md:px-52">
       <div className="h-full w-full flex flex-col items-center justify-center bg-[#1f1f1f] bg-opacity-70 backdrop-blur-md rounded-lg">
         {messages.length === 0 ? (
           <>
@@ -69,13 +69,24 @@ const Gemini: React.FC = () => {
               {messages.map((message, index) => (
                 <div
                   key={index}
-                  className={`mb-4 ${message.type === "ai" ? "ml-auto" : ""}`}
+                  className={`mb-4 flex ${
+                    message.type === "ai" ? "flex-row" : "flex-row md:flex-row-reverse"
+                  }`}
                 >
+                  <img
+                    src={
+                      message.type === "ai"
+                        ? "https://avatar.iran.liara.run/public/87"
+                        : "https://avatars.jakerunzer.com/asdf"
+                    }
+                    alt="avatar"
+                    className="w-10 h-10 rounded-full"
+                  />
                   <div
                     className={`p-4 shadow-lg backdrop-blur-md ${
                       message.type === "ai"
-                        ? "bg-[rgba(34,40,49,0.8)] text-gray-100 max-w-2xl w-auto mr-auto rounded-lg"
-                        : "bg-[rgba(214,90,49,0.8)] text-white max-w-md w-auto ml-auto rounded-3xl"
+                        ? "bg-[rgba(34,40,49,0.8)] text-gray-100 max-w-2xl ml-4 w-auto rounded-lg"
+                        : "bg-transparent md:bg-[rgba(214,90,49,0.8)] text-white max-w-md w-auto ml-4 md:ml-0 mr-0 md:mr-4 rounded-lg md:rounded-3xl"
                     }`}
                   >
                     {message.text}
@@ -88,8 +99,8 @@ const Gemini: React.FC = () => {
         )}
       </div>
 
-      <div className="w-full flex flex-col items-start justify-center h-40 mt-5">
-        <div className="flex space-x-4 mb-4">
+      <div className="w-full flex flex-col items-start justify-center h-20 md:h-40 mt-5">
+        <div className="md:flex space-x-4 mb-4 hidden">
           {[
             "Make Shorter",
             "Make longer",
